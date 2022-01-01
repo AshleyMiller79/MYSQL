e047befa54f1f7fce25420a22853c349eb4acac8
ipek <ipekbilir79@gmail.com>
2022-01-01 20:43:18 +0300

    second

--- day2_select_where.sql
+++ day2_select_where.sql
@@ -237,3 +237,116 @@ Bu örnekte sınav1 veya sınav2 alanı 50'den küçük olan kayıtlar listelene
 
     SELECT * FROM personel
     WHERE isim LIKE 'A_____a%';
+    
+    
+   /* ====================== SELECT - REGEXP_LIKE ================================
+    Daha karmaşık pattern ile sorgulama işlemi için REGEXP_LIKE kullanılabilir.  
+     -- 'c' => case-sentisitive demektir 
+     -- 'i' => incase-sentisitive demektir ve default incase-sensitive aktiftir..
+    Syntax:
+    --------
+    REGEXP_LIKE(sutun_adı, ‘pattern[] ‘, ‘c’ ] ) 
+            
+/* ========================================================================== */
+    use sys;
+    CREATE TABLE kelimeler
+    (
+        id int UNIQUE,
+        kelime VARCHAR(50) NOT NULL,
+        harf_sayisi int
+    );
+    
+    INSERT INTO kelimeler VALUES (1001, 'hot', 3);
+    INSERT INTO kelimeler VALUES (1002, 'hat', 3);
+    INSERT INTO kelimeler VALUES (1003, 'hit', 3);
+    INSERT INTO kelimeler VALUES (1004, 'hbt', 3);
+    INSERT INTO kelimeler VALUES (1005, 'hct', 3);
+    INSERT INTO kelimeler VALUES (1006, 'adem', 4);
+    INSERT INTO kelimeler VALUES (1007, 'selim', 5);
+    INSERT INTO kelimeler VALUES (1008, 'yusuf', 5);
+    INSERT INTO kelimeler VALUES (1009, 'hip', 3);
+    INSERT INTO kelimeler VALUES (1010, 'HOT', 3);
+    INSERT INTO kelimeler VALUES (1011, 'hOt', 3);
+    INSERT INTO kelimeler VALUES (1012, 'h9t', 3);
+    INSERT INTO kelimeler VALUES (1013, 'hoot', 4);
+    INSERT INTO kelimeler VALUES (1014, 'haaat', 5);
+    INSERT INTO kelimeler VALUES (1015, 'hooooot', 5);
+    
+    
+   
+   
+
+    
+    
+    
+/* -----------------------------------------------------------------------------
+  ORNEK22: İçerisinde 'ot' veya 'at' bulunan kelimeleri küçük harfe dikkat
+  ederek listeleyiniz 
+ -----------------------------------------------------------------------------*/ 
+    -- VEYA işlemi için | karakteri kullanılır.
+    
+   SELECT * FROM kelimeler
+    WHERE  REGEXP_LIKE(kelime, 'at|ot','c') ;
+/* -----------------------------------------------------------------------------
+  ORNEK23: İçerisinde 'ot' veya 'at' bulunan kelimeleri büyük-küçük harfe dikkat
+  etmeksizin listeleyeniz
+ -----------------------------------------------------------------------------*/ 
+    SELECT * FROM kelimeler
+    WHERE  REGEXP_LIKE(kelime, 'at|ot'); 
+   
+      
+   
+ /* -----------------------------------------------------------------------------
+  ORNEK24: 'ho' veya 'hi' ile başlayan kelimeleri büyük-küçük harfe dikkat
+  etmeksizin listeleyeniz
+ -----------------------------------------------------------------------------*/   
+     -- Başlangıcı göstermek için ^ karakteri kullanılır.
+    select * from kelimeler
+    where REGEXP_LIKE(kelime, '^ho|^hi');
+    
+    
+/* -----------------------------------------------------------------------------
+  ORNEK25: Sonu 't' veya 'm' ile bitenleri büyük-küçük harfe dikkat
+  etmeksizin listeleyeniz. (i yazmaya gerek yok)
+ -----------------------------------------------------------------------------*/   
+     -- Bitişi göstermek için $ karakteri kullanılır.
+select * from kelimeler
+    where REGEXP_LIKE(kelime,'t$|m$');
+    
+
+/* -----------------------------------------------------------------------------
+  ORNEK26: h ile başlayıp t ile biten 3 harfli kelimeleri (h ile t küçük harfli 
+  olanlari) listeleyeniz
+ -----------------------------------------------------------------------------*/ 
+select * from kelimeler   
+    where REGEXP_LIKE(kelime,'h[a-zA-Z0-9]t','c');
+
+/* -----------------------------------------------------------------------------
+  ORNEK27: İlk harfi h, son harfi t olup 2.harfi a veya i olan 3 harfli 
+  kelimelerin tüm bilgilerini sorgulayalım.
+ -----------------------------------------------------------------------------*/ 
+   select * from kelimeler   
+    where REGEXP_LIKE(kelime,'h[ai]t');
+
+/* -----------------------------------------------------------------------------
+  ORNEK28: İçinde m veya i veya e olan kelimelerin tüm bilgilerini listeleyiniz.
+ -----------------------------------------------------------------------------*/     
+   select * from kelimeler   
+    where REGEXP_LIKE(kelime,'[mie]');-- m|i|e
+ 
+/* -----------------------------------------------------------------------------
+  ORNEK29: a veya s ile başlayan kelimelerin tüm bilgilerini listeleyiniz.
+-----------------------------------------------------------------------------*/ 
+ select * from kelimeler   
+    where REGEXP_LIKE(kelime,'[as]');
+
+/* -----------------------------------------------------------------------------
+  ORNEK30: içerisinde en az 2 adet oo barıdıran kelimelerin tüm bilgilerini 
+  listeleyiniz.
+-----------------------------------------------------------------------------*/ 
+  
+    select * from kelimeler   
+    where REGEXP_LIKE(kelime,'[o]{2}');
+    
+    
+    
---
+++ day3_constraints.sql
@@ -0,0 +1,105 @@
+/*============== CONSTRAINTS - KISITLAMALAR ======================================
+           
+    NOT NULL - Bir Sütunun  NULL içermemesini garanti eder. 
+    UNIQUE - Bir sütundaki tüm değerlerin BENZERSİZ olmasını garanti eder.  
+    PRIMARY KEY - Bir sütünün NULL içermemesini ve sütundaki verilerin 
+                  BENZERSİZ olmasını garanti eder.(NOT NULL ve UNIQUE birleşimi gibi)
+    FOREIGN KEY - Başka bir tablodaki Primary Key’i referans göstermek için kullanılır. 
+                  Böylelikle, tablolar arasında ilişki kurulmuş olur. 
+                  foreign ve primary ile tablolar birbirine bağlamadan da, id leri ayni olan satirlarda işlem yap diyebiliriz, böylelikle
+                  ayni field verilebilir, parent child ilişkisi olmamiş olur
+    CHECK - Bir sutundaki tüm verilerin belirlenen özel bir şartı sağlamasını garanti eder. 
+    DEFAULT - Herhangi bir değer atanamadığında Başlangıç değerinin atanmasını sağlar.
+ ========================================================================================*/
+ CREATE TABLE calisanlar
+    (
+        id CHAR(5) PRIMARY KEY, -- not null+ unique
+        isim VARCHAR(50) UNIQUE,
+        maas int NOT NULL,
+        ise_baslama DATE
+    );
+ INSERT INTO calisanlar VALUES( '10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
+   INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
+   INSERT INTO calisanlar VALUES('10010', null, 5000, '2018-04-14');-- 2. kez null benzersizlik kisitlamasini bozmaz
+    INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
+    INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14'); 
+    INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12'); -- maas null olamaz
+    INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
+	INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14');-- can zaten var
+    INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14');-- '' bunu int kabul etmez
+    INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');-- '' bunu (hiçlik) string kabul eder
+     INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14'); -- id tekrarl olamaz primary key 
+      INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14');-- primary key old. null yazamayz
+      
+      select * from calisanlar;
+      
+      
+      /*------------
+-- KISITLAMALAR (  FOREIGN KEY)
+----------------------------------------------------------------*/
+      
+    CREATE TABLE adresler
+    (
+        adres_id CHAR(5),     
+        sokak VARCHAR(50),
+        cadde VARCHAR(30),
+        sehir VARCHAR(15),
+        CONSTRAINT id_clsnlr FOREIGN KEY(adres_id) REFERENCES calisanlar(id) -- (solda tablo seçiliyken üstteki info dan ismi kontrol edebilirsin)
+    );
+    
+    INSERT INTO adresler VALUES('10003','Mutlu Sok', '40.Cad.','IST');
+    INSERT INTO adresler VALUES('10003','Can Sok', '50.Cad.','Ankara');
+    INSERT INTO adresler VALUES('10002','Ağa Sok', '30.Cad.','Antep');
+    -- parent tabloda olmayan id ile child a ekleme yapamayiz
+    
+    -- FK'ye null değeri atanabilir. 
+    INSERT INTO adresler VALUES(NULL,'Ağa Sok', '30.Cad.','Antep');
+      
+      select * from adresler;
+      -- bağlantl tablolarda child silinmeden parent silinmez
+      drop table calisanlar;
+      drop table adresler; 
+      
+      
+      /*============================== ON DELETE CASCADE =============================
+
+  Her defasında önce child tablodaki verileri silmek yerine ON DELETE CASCADE
+  silme özelliğini aktif hale getirebiliriz.
+  
+  Bunun için FK olan satırın en sonuna ON DELETE CASCADE komutunu yazmak yeterli
+  
+==============================================================================*/ 
+   CREATE TABLE talebeler
+    (
+        id CHAR(3) primary key,  
+        isim VARCHAR(50),
+        veli_isim VARCHAR(50),
+        yazili_notu int
+    );
+    
+    INSERT INTO talebeler VALUES(123, 'Ali Can', 'Hasan',75);
+    INSERT INTO talebeler VALUES(124, 'Merve Gul', 'Ayse',85);
+	INSERT INTO talebeler VALUES(125, 'Kemal Yasa', 'Hasan',85);
+    INSERT INTO talebeler VALUES(126, 'Nesibe Yılmaz', 'Ayse',95);
+	INSERT INTO talebeler VALUES(127, 'Mustafa Bak', 'Can',99);
+
+     CREATE TABLE notlar 
+    ( talebe_id char(3), 
+        ders_adi varchar(30), 
+        yazili_notu int, 
+        CONSTRAINT notlar_fk FOREIGN KEY (talebe_id) 
+        REFERENCES talebeler(id) ON DELETE CASCADE); -- on delete cascade sayesinde
+       -- parent taki silinen bir kayıt ile ilişkili olan tüm child kayıtlarını
+       -- siler.  
+       -- mesela bir hastane silindi o hastanedeki bütün kayıtlar silinmeli, oda böyle olur.
+       -- cascade yoksa önce child temizlenir sonra parent
+    
+
+    INSERT INTO notlar VALUES ('123','kimya',75);
+    INSERT INTO notlar VALUES ('124', 'fizik',65);
+    INSERT INTO notlar VALUES ('125', 'tarih',90);
+	INSERT INTO notlar VALUES ('126', 'Matematik',90);   
+      
+      drop table talebeler;
+      
+      
