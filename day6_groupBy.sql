e711b6027d9120811c453a6adc635f0aafff4f36
İpek Bilgin <81853731+ipekbilir@users.noreply.github.com>
2022-01-07 11:06:52 +0300

    Update day6_groupBy.sql

--- day6_groupBy.sql
+++ day6_groupBy.sql
@@ -28,42 +28,111 @@
     INSERT INTO manav VALUES( 'Veli', 'Elma', 3);
     INSERT INTO manav VALUES( 'Ayse', 'Uzum', 4);
     INSERT INTO manav VALUES( 'Ali', null, 2);
-    
-    
-    /* -----------------------------------------------------------------------------
+    select * from manav;
+/* -----------------------------------------------------------------------------
   ORNEK1: kisi ismine göre satılan toplam meyve miktarlarını gösteren sorguyu 
   yazınız.
 ------------------------------------------------------------------------------*/  
-    select isim, sum(urun_miktari) toplam_urun from manav
-    group by isim;
-    
-    /* ----------------------------------------------------------------------------
+    SELECT isim, SUM(urun_miktari)   toplam_urun 
+    FROM manav
+    GROUP BY isim;
+   
+/* ----------------------------------------------------------------------------
   ORNEK2: satılan meyve türüne (urun_adi) göre urun alan kişi sayısını gösteren
   sorguyu yazınız. NULL olarak girilen meyveyi listelemesin.
 -----------------------------------------------------------------------------*/ 
-select urun_adi, count(isim) kisi_sayisi
-from manav
-where urun_adi is not null
-group by urun_adi;
--- group by ile birlikte where cümleciği kullanilabilir
-
+    -- GROUP by ile birlikte WHERE cümleciği kullanılabilir.
+        
+    SELECT urun_adi, COUNT(isim) AS kisi_sayisi
+    FROM manav
+    WHERE urun_adi IS NOT NULL
+    GROUP BY urun_adi;
+    
+    SELECT * FROM manav;
+    
+   
 /* ----------------------------------------------------------------------------
   ORNEK3: satılan meyve türüne (urun_adi) göre satılan (urun_miktari )MIN ve 
   MAX urun miktarlarini, MAX urun miktarina göre sıralayarak listeyen sorguyu 
   yazınız.
 -----------------------------------------------------------------------------*/ 
-select urun_adi, min(urun_miktari) min, max(urun_miktari) maks
-from manav
-where urun_adi is not null
-group by urun_adi
-order by maks;
-
+    SELECT urun_adi,MIN(urun_miktari) min_miktar, MAX(urun_miktari) maks_miktar
+    FROM manav
+    WHERE urun_adi IS NOT NULL
+    GROUP BY urun_adi
+    ORDER BY maks_miktar ;
+    
 /* ----------------------------------------------------------------------------
   ORNEK4: kisi ismine ve urun adına göre satılan ürünlerin toplamını 
   gruplandıran ve isime göre ters sırasıda listeyen sorguyu yazınız.
  -----------------------------------------------------------------------------*/ 
-select isim, urun_adi, sum(urun_miktari) toplam_miktar
-from manav
-group by isim,urun_adi
-order by isim desc;
+     SELECT isim, urun_adi, SUM(urun_miktari) as toplam_miktar
+     FROM manav
+   --  WHERE urun_adi IS NOT NULL
+     GROUP BY isim, urun_adi
+     ORDER BY isim DESC;
+    
+   /* ----------------------------------------------------------------------------
+  ORNEK5: kisi ismine ve urun adına göre (gruplayarak) satılan ürünlerin toplamını bulan ve
+  ve bu toplam değeri 3 ve fazlası olan kayıtları toplam urun miktarlarina göre
+  ters siralayarak listeyen sorguyu yazınız.
+ -----------------------------------------------------------------------------*/  
+ select isim, urun_adi, sum(urun_miktari)
+ from manav
+ group by isim, urun_adi
+ having sum(urun_miktari)>=3
+ order by sum(urun_miktari) desc;
+ 
+ -- aggregate fonksiyonlari ile ilgili bir koşul koymak için 
+ -- group by dan sonra having cümleciği kullanilir.
+ 
+  /* ----------------------------------------------------------------------------
+  ORNEK6: satılan urun_adi'na göre (gruplayarak) MAX urun sayılarını sıralayarak listeleyen 
+  sorguyu yazınız. NOT: Sorgu, sadece MAKS urun_miktari MIN urun_miktarına 
+  eşit olmayan kayıtları listelemelidir.
+ -----------------------------------------------------------------------------*/    
+ select urun_adi, max(urun_miktari) maks
+ from manav
+ group by urun_adi
+ having maks!=min(urun_miktari) -- != yerine <> kullanilabilir
+ order by maks;
+ -- sralama group by
+ -- having
+ -- order by
+ 
+ /*============================= DISTINCT =====================================
+    
+    DISTINCT cümleciği bir SORGU ifadesinde benzer olan satırları filtrelemek
+    için kullanılır. Dolayısıyla seçilen sutun yada sutunlar için benzersiz veri
+    içeren satırlar oluşturmaya yarar.
+   
+    SYNTAX
+    -------
+    SELECT DISTINCT sutun_adi1, sutun_adi2, sutun_adi3
+    FROM  tablo_adı;
+==============================================================================*/
+    
+  /* ----------------------------------------------------------------------------
+  ORNEK1: satılan farklı meyve türlerinin sayısını listeleyen sorguyu yazınız.
+-----------------------------------------------------------------------------*/     
+    select count(distinct urun_adi) from manav;
+    
+    /* -----------------------------------------------------------------------------
+  ORNEK2: satılan meyve + isimlerin farklı olanlarını listeyen sorguyu yazınız.
+------------------------------------------------------------------------------*/
+select distinct urun_adi,isim
+from manav;
 
+/* ----------------------------------------------------------------------------
+  ORNEK3: satılan meyvelerin urun_miktarlarinin benzersiz  olanlarının 
+  toplamlarini listeyen sorguyu yazınız.
+-----------------------------------------------------------------------------*/  
+  select sum(distinct urun_miktari) urun_sayisi
+  from manav;
+    
+    
+    
+    
+    
+    
+    
